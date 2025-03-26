2513163ae9ec6ca193ec5a0d7ed132f9f6d9f347
sibusisomakaula <217101887@mycput.ac.za>
2025-03-26 21:22:31 +0200

    my base code updated base code with factory

---
+++ src/main/java/ac/za/cput/factory/courseFactory.java
@@ -0,0 +1,11 @@
+package ac.za.cput.factory;
+
+import ac.za.cput.domain.Course;
+
+public class courseFactory {
+    public static Course createCouse(String courseName, String courseId, String lectureId, String departmentId) {
+     return null;
+    }
+
+
+}
