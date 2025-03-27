db5515087deb0becad96ecad8d3b556e57407b0f
Unathi Keke <keke.ac.za.cput@gmail.com>
2025-03-27 17:52:13 +0200

    fixed spelling error

--- src/main/java/ac/za/cput/factory/courseFactory.java
+++ src/main/java/ac/za/cput/factory/courseFactory.java
@@ -3,7 +3,7 @@ package ac.za.cput.factory;
 import ac.za.cput.domain.Course;
 
 public class courseFactory {
-    public static Course createCouse(String courseName, String courseId, String lectureId, String departmentId) {
+    public static Course createCourse(String courseName, String courseId, String lectureId, String departmentId) {
      return null;
     }
 
