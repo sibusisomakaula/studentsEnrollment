28aca44e5b40e521c208545b62a4ce17cf2ed503
sibusisomakaula <217101887@mycput.ac.za>
2025-03-27 20:32:57 +0200

    Update courseFactory.java

--- src/main/java/ac/za/cput/factory/courseFactory.java
+++ src/main/java/ac/za/cput/factory/courseFactory.java
@@ -2,10 +2,14 @@ package ac.za.cput.factory;
 
 import ac.za.cput.domain.Course;
 
-public class courseFactory {
-    public static Course createCourse(String courseName, String courseId, String lectureId, String departmentId) {
-     return null;
+public static Course createCourse(String courseName, String courseId, String lecturerId,String departmentId) {
+        if (Helper.isNullOrEmpty(courseName) || Helper.isNullOrEmpty(courseId) || Helper.isNullOrEmpty(lecturerId)|| Helper.isNullOrEmpty(departmentId))
+            return null;
+
+        return new Course.Builder().setCourseName(courseName)
+                .setCourseId(courseId)
+                .setLecturerId(lecturerId)
+                .setDepartmentId(departmentId)
+                .build();
     }
-
-
 }
