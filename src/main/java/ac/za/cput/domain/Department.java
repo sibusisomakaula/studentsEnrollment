87955586bcf0695f89191db616e148ef3c53d2e2
Junior Mageza <130604010+2222864273@users.noreply.github.com>
2025-03-27 19:00:08 +0200

    Add files via upload

---
+++ src/main/java/ac/za/cput/domain/Department.java
@@ -0,0 +1,55 @@
+package ac.za.cput.domain;
+
+import java.lang.module.ModuleDescriptor;
+import java.util.Locale;
+
+public class Department {
+    private String departmentName;
+    private String departmentId;
+
+
+    private Department() {
+    }
+    private  Department(Builder builder){
+        this.departmentName = builder.departmentName;
+        this.departmentId = builder.departmentId;
+    }
+
+    public String getDepartmentName() {
+        return DepartmentName;
+    }
+
+    public String getDepartmentId() {
+        return DeparmentId;
+    }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "departmentName='" + departmentName + '\'' +
+                ", departmentId='" + departmentId + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private String departmentName;
+        private String departmentId;
+
+        public Builder setDepartmentName(String departmentName) {
+            this.departmentName = departmentName;
+            return this;
+        }
+        public Builder setDepartmentId(String departmentId) {
+            this.departmentId = departmentId;
+            return this;
+        }
+        public Department build(){
+
+            return new Department(this);}
+
+    }
+}
