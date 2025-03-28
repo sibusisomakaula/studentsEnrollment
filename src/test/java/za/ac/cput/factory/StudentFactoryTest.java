a08bfd4086d55184072f9f73e6d90c7c653058e8
Coben220235686 <220235686@mycput.ac.za>
2025-03-28 21:19:18 +0200

    Update StudentFactoryTest.java

--- src/test/java/za/ac/cput/factory/StudentFactoryTest.java
+++ src/test/java/za/ac/cput/factory/StudentFactoryTest.java
@@ -10,32 +10,32 @@ import ac.za.cput.domain.Student;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class LibraryFactoryTest {
+class StudentFactoryTest {
 
     private static Student l1 = StudentFactory.createStudent("1245","Amy", "Good",
-            "222d", "123");
+            "2223", "123");
     private static Student l2 = StudentFactory.createStudent("123456","Sinolo", "Pasta",
-            "45566", "1235");
+            "455566", "1235");
     private static Student l3 = StudentFactory.createStudent("1245","Cobey", "Gomez",
-            "78566", "7654321098");
+            "785578", "7654321098");
 
     @Test
     @Order(1)
-    public void testCreateLibrary() {
+    public void testCreateStudent() {
         assertNotNull(l1);
         System.out.println(l1);
     }
 
     @Test
     @Order(2)
-    public void testCreateLibraryWithAllAttributes() {
+    public void testCreateStudentWithAllAttributes() {
         assertNotNull(l2);
         System.out.println(l2.toString());
     }
 
     @Test
     @Order(3)
-    public void testCreateLibraryThatWillFail() {
+    public void testCreateStudentThatWillFail() {
         assertNull(l3);
         System.out.println("Student creation failed as expected.");
     }
