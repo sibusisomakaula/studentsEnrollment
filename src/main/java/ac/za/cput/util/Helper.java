ade8c3a7a6917ad0adee44a5243a3ae70a9fb0e2
Coben220235686 <220235686@mycput.ac.za>
2025-03-28 13:23:00 +0200

    Add files via upload

--- src/main/java/ac/za/cput/util/Helper.java
+++ src/main/java/ac/za/cput/util/Helper.java
@@ -1,17 +1,10 @@
-package ac.za.cput.util;
-
-import java.util.UUID;
+package za.ac.cput.util;
 
 public class Helper {
-    public static boolean isNullOrEmpty(String value){
-        return value == null || value.trim().isEmpty();
+
+    public static boolean isNullOrEmpty(String str) {
+        if (str == null || str == null)
+            return true;
+        return false;
     }
-    public static String generateId(){
-        return UUID.randomUUID().toString();
-
-    }
-
-    }
-
-
-
+}
