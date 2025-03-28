cc42c79820dc1f850486054f416d5c61563093cc
Coben220235686 <220235686@mycput.ac.za>
2025-03-28 13:33:41 +0200

    Add files via upload

--- src/main/java/ac/za/cput/Main.java
+++ src/main/java/ac/za/cput/Main.java
@@ -1,8 +1,10 @@
-package ac.za.cput;
+package za.ac.cput;
+
+import za.ac.cput.domain.Student;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        Student student = new Student.Builder().studentID("12345").firstName("Marcus") .lastName("Luther").studentEmail("marcus.luther@email.com").departmentID("CS11").build();
+        System.out.println(student);
     }
 }
