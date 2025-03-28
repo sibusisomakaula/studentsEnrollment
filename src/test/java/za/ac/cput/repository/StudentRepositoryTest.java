eea2a6e88581a11151c77c9d919dc4949614e270
Coben220235686 <220235686@mycput.ac.za>
2025-03-28 21:06:14 +0200

    Add files via upload

---
+++ src/test/java/za/ac/cput/repository/StudentRepositoryTest.java
@@ -0,0 +1,53 @@
+package ac.za.cput.repository;
+
+import ac.za.cput.domain.Student;
+import ac.za.cput.factory.StudentFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+class LibraryRepositoryTest {
+
+    private static IStudentRepository repository = StudentRepository.getInstance();
+    private Student student = StudentRepository.createStudent("cputlibrary120", "IThala Lencwadi", "cputlib@gmail.com", "Hanover St, D6 Campus, Cape Town, 8000", "01");;
+
+
+    @Test
+    void create() {
+        Student create = repository.create(student);
+        System.out.println(create);
+    }
+
+    @Test
+    void read() {
+        Student create = repository.create(student);
+        Student read = repository.read(create.getStudentID());
+        System.out.println("Id: " + Student.getStudentID());
+        assertNotNull(read);
+    }
+
+    @Test
+    void update() {
+        Student updateStudent = new Student().Builder().copy(student).setStudentEmail("cput@gmail.com").build();
+        Student createStudent = repository.create(create());
+        Student updatedStudent = repository.update(createStudent);
+        assertNotNull(updatedStudent);
+        System.out.println("New Library: " + updatedStudent);
+    }
+
+    @Test
+    void delete() {
+        Student create = repository.create(student);
+        assertTrue(repository.delete(create.getStudentID()));
+    }
+
+    @Test
+    void getAll() {
+        Student create = repository.create(student);
+        List<Student> StudentList = repository.getAll(create);
+        assertNotNull(StudentList);
+        System.out.println(StudentList);
+    }
+}
