a5617b4fc79cc05ab4b4600c4248ea27faf1008f
Coben220235686 <220235686@mycput.ac.za>
2025-03-28 19:54:39 +0200

    Add files via upload

---
+++ src/test/java/za/ac/cput/factory/StudentFactoryTest.java
@@ -0,0 +1,42 @@
+package ac.za.cput.factory;
+
+import ac.za.cput.factory.StudentFactory;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import ac.za.cput.domain.Student;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class LibraryFactoryTest {
+
+    private static Student l1 = StudentFactory.createStudent("1245","Amansa", "Madapa",
+            "222d", "123");
+    private static Student l2 = StudentFactory.createStudent("123456","Sino", "Lasta",
+            "45566", "1235");
+    private static Student l3 = StudentFactory.createStudent("1245","Cobe", "Rmafsd",
+            "78566", "7654321098");
+
+    @Test
+    @Order(1)
+    public void testCreateLibrary() {
+        assertNotNull(l1);
+        System.out.println(l1);
+    }
+
+    @Test
+    @Order(2)
+    public void testCreateLibraryWithAllAttributes() {
+        assertNotNull(l2);
+        System.out.println(l2.toString());
+    }
+
+    @Test
+    @Order(3)
+    public void testCreateLibraryThatWillFail() {
+        assertNull(l3);
+        System.out.println("Student creation failed as expected.");
+    }
+}
