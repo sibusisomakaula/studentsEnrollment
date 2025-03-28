3c7958a3039674e04873af920342219a7641d4cd
Junior Mageza <130604010+2222864273@users.noreply.github.com>
2025-03-28 20:19:15 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/repository/DepartmentRepository.java
@@ -0,0 +1,70 @@
+package ac.za.cput.repository;
+
+import ac.za.cput.domain.Department;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DepartmentRepository implements IDepartmentRepository {
+
+    private static IDepartmentRepository repository = null;
+    private List<Department>departmentList;
+
+
+    private DepartmentRepository(){
+        departmentList = new ArrayList<Department>();
+
+    }
+    public static IDepartmentRepository getRepository(){
+        if(repository == null){
+            repository = new DepartmentRepository();
+        }
+        return repository;
+    }
+
+    @Override
+    public Department create(Department department) {
+        boolean success = department.add(department);
+        if(success){
+            return department;
+        }
+        return null;
+    }
+
+    @Override
+    public Department read(String departmentid) {
+        for(Department d : departmentList){
+            if(d.getDepartmentId().equals(departmentid))
+                return d;
+        }
+        return null;
+    }
+
+    @Override
+    public Department update(Department department) {
+        String departmentid = department.getDepartmentId();
+        Department departmentcold = read(departmentid);
+        if (Departmentold == null)
+            return null;
+        boolean success = delete(departmentid);
+        if (success) {
+            if (departmentList.add(department));
+
+        }
+        return department;
+    }
+
+
+    @Override
+    public boolean delete(String departmentid) {
+        Department departmentToDelete = read(departmentid);
+        if(departmentToDelete == null)
+            return false;
+        return(departmentList.remove(departmentToDelete));
+    }
+
+    @Override
+    public List<Department> getAll(Department departmentAll) {
+        return departmentList;
+    }
+}
