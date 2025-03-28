111ff27735ca12bda0359e4ce3222618fcfc11a3
Coben220235686 <220235686@mycput.ac.za>
2025-03-28 20:54:55 +0200

    Update StudentFactory.java

--- src/main/java/za/ac/cput/factory/StudentFactory.java
+++ src/main/java/za/ac/cput/factory/StudentFactory.java
@@ -2,7 +2,7 @@ package za.ac.cput.factory;
 
 import za.ac.cput.domain.Student;
 
-
+import za.ac.cput.util.Helper
 
 public class StudentFactory {
 
@@ -11,21 +11,22 @@ public class StudentFactory {
                                         String lastName,
                                         String studentEmail,
                                         String departmentID) {
-        if (studentID == null || studentID.isEmpty() ||
-                firstName == null || firstName.isEmpty() ||
-                lastName == null || lastName.isEmpty() ||
-                studentEmail == null || studentEmail.isEmpty() ||
-                departmentID == null || departmentID.isEmpty())
+        if (Helper.isNullOrEmpty(studentID) ||
+            Helper.NullOrEmpty(firstName) ||
+                 Helper.isNullOrEmpty(lastName) ||
+                Helper.isNullOrEmpty(studentEmail) ||
+                 Helper.isNullOrEmpty(departmentID))
+                 
         {
             return null;
         }
 
         return new Student.Builder()
-                .studentID(studentID)
-                .firstName(firstName)
-                .lastName(lastName)
-                .studentEmail(studentEmail)
-                .departmentID(departmentID)
+                .setStudentName(studentID)
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setStudentEmail(studentEmail)
+                .setDepartmentID(departmentID)
                 .build();
     }
 
